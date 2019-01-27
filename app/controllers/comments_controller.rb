9bd1ed2358ac0b8a06c0f7172a2d770f47326745
FotoVerite <mzbphoto@gmail.com>
2019-01-27 15:15:04 -0500

    adds active scope to comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,12 @@ class CommentsController < ApplicationController
 
   def index
     @conversation = @current_user.organization.conversations.find(params[:conversation_id])
-    @comments = @conversation.comments
+    @comments = @conversation.comments.active
   end
 
   def create
     @current_user.comments.create(
-      content: params[:content], 
+      content: params[:content],
       conversation_id: params[:conversation_id],
       tid: params[:tid],
       votable: true)
